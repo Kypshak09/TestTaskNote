398e4166fb0548af48a721021b2dd52b3d99d45a
Kypshak09 <amirzhunusov09@gmail.com>
2023-02-16 20:10:03 +0600

    Initial Commit

---
+++ TestTaskNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskNote
+//
+//  Created by Amir Zhunussov on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
