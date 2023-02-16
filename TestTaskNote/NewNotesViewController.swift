137d66ae62afda06cdd87004294fd257368e7ce2
Kypshak09 <amirzhunusov09@gmail.com>
2023-02-16 21:06:07 +0600

    toolbar with buttons, but not working with selected text

--- TestTaskNote/NewNotesViewController.swift
+++ TestTaskNote/NewNotesViewController.swift
@@ -19,6 +19,20 @@ class NewNotesViewController: UIViewController {
     let realm = try! Realm()
     var array: Results<Note>!
     
+    let viewToolBar: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.961, green: 0.961, blue: 0.863, alpha: 1)
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    let toolBar: UIToolbar = {
+    let toolbar = UIToolbar()
+    toolbar.barTintColor = UIColor(red: 0.961, green: 0.961, blue: 0.863, alpha: 1)
+    toolbar.tintColor = UIColor(red: 1, green: 0.482, blue: 0.329, alpha: 1)
+    return toolbar
+    }()
+    
     let textView: UITextView = {
         let text = UITextView()
         text.layer.cornerRadius = 15
@@ -35,6 +49,7 @@ class NewNotesViewController: UIViewController {
             textView.text = note.text
             }
         configureConstraint()
+        configureToolbar()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -57,15 +72,95 @@ class NewNotesViewController: UIViewController {
     }
     
     func configureConstraint() {
+        
+        view.addSubview(viewToolBar)
+        viewToolBar.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(80)
+            make.trailing.equalToSuperview().offset(-80)
+            make.height.equalTo(60)
+        }
+        
+        viewToolBar.addSubview(toolBar)
+        toolBar.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.height.equalTo(50)
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
         view.addSubview(textView)
         textView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(200)
+            make.top.equalToSuperview().offset(150)
             make.leading.equalToSuperview().offset(7)
             make.trailing.equalToSuperview().offset(-7)
             make.bottom.equalToSuperview().offset(-80)
         }
     }
     
+    func configureToolbar() {
+           let boldButton = UIBarButtonItem(title: "B", style: .plain, target: self, action: #selector(boldButtonPressed))
+           let italicButton = UIBarButtonItem(title: "I", style: .plain, target: self, action: #selector(italicButtonPressed))
+           let monoSpaceButton = UIBarButtonItem(title: "M", style: .plain, target: self, action: #selector(monoSpaceButtonPressed))
+           let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+           toolBar.setItems([boldButton, spacer, italicButton, spacer, monoSpaceButton], animated: false)
+       }
+    
+    @objc func boldButtonPressed() {
+        var attributes = textView.typingAttributes
+            var isBold = false
+            if let font = attributes[NSAttributedString.Key.font] as? UIFont {
+                if let fontDescriptor = font.fontDescriptor.withSymbolicTraits(.traitBold) {
+                    isBold = true
+                    let boldFont = UIFont(descriptor: fontDescriptor, size: font.pointSize)
+                    attributes[NSAttributedString.Key.font] = boldFont
+                }
+            }
+            if !isBold {
+                let boldFont = UIFont.boldSystemFont(ofSize: 15)
+                attributes[NSAttributedString.Key.font] = boldFont
+            }
+            textView.typingAttributes = attributes
+    }
+    
+    @objc func italicButtonPressed() {
+        
+        var attributes = textView.typingAttributes
+            var isItalic = false
+            if let font = attributes[NSAttributedString.Key.font] as? UIFont {
+                if let fontDescriptor = font.fontDescriptor.withSymbolicTraits(.traitItalic) {
+                    isItalic = true
+                    let italicFont = UIFont(descriptor: fontDescriptor, size: font.pointSize)
+                    attributes[NSAttributedString.Key.font] = italicFont
+                }
+            }
+            if !isItalic {
+                let italicFont = UIFont.boldSystemFont(ofSize: 15)
+                attributes[NSAttributedString.Key.font] = italicFont
+            }
+            textView.typingAttributes = attributes
+        
+    }
+    
+    @objc func monoSpaceButtonPressed() {
+        
+        var attributes = textView.typingAttributes
+            var isMono = false
+            if let font = attributes[NSAttributedString.Key.font] as? UIFont {
+                if let fontDescriptor = font.fontDescriptor.withSymbolicTraits(.traitMonoSpace) {
+                    isMono = true
+                    let monoFont = UIFont(descriptor: fontDescriptor, size: font.pointSize)
+                    attributes[NSAttributedString.Key.font] = monoFont
+                }
+            }
+            if !isMono {
+                let monoFont = UIFont.boldSystemFont(ofSize: 15)
+                attributes[NSAttributedString.Key.font] = monoFont
+            }
+            textView.typingAttributes = attributes
+        
+    }
 }
 
 extension NewNotesViewController: UITextViewDelegate {
